89f099ecb22d106555ee74e78a1bdbc8d10eae28
Utee Ekong <utibeabasiekong2@gmail.com>
2024-11-21 12:28:54 +0100

    update: add helemt

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
-import { DolphFactory } from "@dolphjs/dolph";
+import { DolphFactory, middlewareRegistry } from "@dolphjs/dolph";
 import { AccountComponent } from "./components/account/account.component";
 import { QuizComponent } from "./components/quiz/quiz.component";
+import helmet from "helmet";
+
+middlewareRegistry.register(helmet());
 
 const dolph = new DolphFactory([AccountComponent, QuizComponent]);
 dolph.start();
